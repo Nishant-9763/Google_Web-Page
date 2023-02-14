337a50f3da394d263c56e51b4ef450d85eb0f4f7
Nishnat-9763 <nishnatgupta9763@gmail.com>
2023-02-14 21:58:12 +0530

    Done-1.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const Logo = () => {
+  return <div style={{display:'flex' , justifyContent:'center'}}>
+    <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt='google logo' width={300} height={100}
+    />
+    </div>
+}
+
+const WordBox = () =>{
+  return <div  style={{display:'flex' , justifyContent:'center',borderRadius:'500px' ,height:'40px',tabSize:'100px'}}>
+    <input  size={80} />
+  </div>
+}
+
+const Button = ()=>{
+  return <div style={{display:'flex' , justifyContent:'center' , margin:'8px' ,gap:'5px'}}>
+    <button>Google Search</button> <button>I'm Feeling Lucky</button>
+  </div>
+}
+
+const LanguageNote = ()=>{
+  return <p style={{display:'flex' , justifyContent:'center'}}>Google ca offered in :<a href='#'>Francias</a> </p>
+}
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <div style={{margin:'150px'}} >
+  <Logo/>
+   <WordBox/>
+   <Button/>
+   <LanguageNote/>
+    </div> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
